06db726a7fdd6bbd6dca02e30751a524164422bf
Dylan R Harp <dharp73@gmail.com>
2021-07-02 15:32:12 -0600

    Got field.py to run by seting configs for images, etc.

--- samples/field/create_image_tiles.py
+++ samples/field/create_image_tiles.py
@@ -4,23 +4,30 @@ from rasterio.mask import mask
 from rasterio.plot import show
 from shapely import geometry
 from glob import glob
+from numpy.random import rand
 
 # Data folder: https://thefreshwatertrustorg-my.sharepoint.com/personal/dion_thefreshwatertrust_org/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fdion%5Fthefreshwatertrust%5Forg%2FDocuments%2FDocuments%2FGrandview%5FOrtho%5F2019&originalPath=aHR0cHM6Ly90aGVmcmVzaHdhdGVydHJ1c3RvcmctbXkuc2hhcmVwb2ludC5jb20vOmY6L2cvcGVyc29uYWwvZGlvbl90aGVmcmVzaHdhdGVydHJ1c3Rfb3JnL0VtOGhURS1zVkJOQ2lUTjdvVTBxN1JZQmJ3VjdISGhsSllrcWhzblJpaldPdnc%5FcnRpbWU9aEp1Smp2QTcyVWc
 
-outfile = r'train/gvtest_{}.tif'
-
 # Collect shape geometries from shapefile
-sf = fiona.open("/Users/dharp/Data/GrandView/Grandview_fields/Grandview_fields_DW.shp", "r")
+sf = fiona.open("Grandview_fields/Grandview_fields_DW.shp", "r")
 
 # Open raster image file
 #rf = rasterio.open("/Users/dharp/Desktop/gv_1500dpi.tif")
-rf = rio.open("/Users/dharp/Data/GrandView/Grandview_1500dpi.tif")
+rf = rio.open("Grandview_1500dpi.tif")
 
 # Set buffer
-N = 2000 # pixels
+N = 2048 # pixels
 
 for s in sf:
+    if s["geometry"]["type"] == "MultiPolygon": continue
+    if len(s["geometry"]["coordinates"]) != 1: continue
 
+    # Split into training and validation sets
+    if rand() <= 0.8:
+        outfile = r'train/gvclip_shapeid{}.tif'
+    else:
+        outfile = r'val/gvclip_shapeid{}.tif'
+    
     # Get pixel ids at center of shape
     pshape = geometry.Polygon(s["geometry"]["coordinates"][0])
     xc = pshape.centroid.coords.xy[0][0]
--- samples/field/field.py
+++ samples/field/field.py
@@ -48,13 +48,19 @@ class FieldConfig(Config):
     IMAGES_PER_GPU = 2
 
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 1  # Background + balloon
+    NUM_CLASSES = 4  # Background + balloon
 
     # Number of training steps per epoch
     STEPS_PER_EPOCH = 100
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
+
+    IMAGE_MAX_DIM = 2048
+
+    IMAGE_MIN_DIM = 2048
+
+    IMAGE_SHAPE = [2048, 2048, 3]
 
 
 ############################################################
@@ -63,13 +69,13 @@ class FieldConfig(Config):
 
 class FieldDataset(utils.Dataset):
 
-    def load_fields(self, dataset_dir, subset):
+    def load_fields(self, dataset_dir, subset, shapefile):
         """Load a subset of the Balloon dataset.
         dataset_dir: Root directory of the dataset.
         subset: Subset to load: train or val
         """
         # Add classes. We have only one class to add.
-        self.add_class("field", 1, "Not irrigated")
+        self.add_class("field", 1, "Not Irrigated")
         self.add_class("field", 2, "Flood")
         self.add_class("field", 3, "Sprinkler")
 
@@ -79,7 +85,7 @@ class FieldDataset(utils.Dataset):
         dataset_dir = os.path.join(dataset_dir, subset)
 
         # Open shapefile containing field polygons
-        sf = fiona.open(os.path.join(dataset_dir,"graind_view_2019_training2/graind_view_2019_training2.shp"), "r")
+        sf = fiona.open(shapefile, "r")
 
         # Loop through geotiffs in folder
         for tf in glob(os.path.join(dataset_dir,"*.tif")):
@@ -98,6 +104,8 @@ class FieldDataset(utils.Dataset):
 
             # Loop through field polygons in shapefile
             for p in sf:
+                # Skip polygons with more than 1 set of coordinates
+                if len(p['geometry']['coordinates']) != 1: continue
                 # Ignore 'MultiPolygon' type for now
                 if p['geometry']['type'] == 'Polygon':
                     psf = geometry.Polygon(p['geometry']['coordinates'][0])
@@ -136,7 +144,11 @@ class FieldDataset(utils.Dataset):
             out_image, out_transform = mask(rf, [psf], invert=True)
             out_mask[:,:,i] = out_image[3,:,:]
             rf.close()
-            class_ids.append(self.class_names.index(p['properties']['Irrigation']))
+            if p['properties']['_irrType'] is None:
+                irrType = "Not Irrigated"
+            else:
+                irrType = p['properties']['_irrType']
+            class_ids.append(self.class_names.index(irrType))
 
         out_mask[out_mask==0] = 1
         out_mask[out_mask==255] = 0
@@ -161,21 +173,66 @@ def get_raster_bounds(rf):
     for geom, val in rasterio.features.shapes(mask, transform=rf.transform):
         # Transform shapes from the dataset's own coordinate
         # reference system to CRS84 (EPSG:4326).
-        geom = rasterio.warp.transform_geom(rf.crs, 'EPSG:4326', geom, precision=6)
+        #geom = rasterio.warp.transform_geom(rf.crs, 'EPSG:4326', geom, precision=6)
+        geom = rasterio.warp.transform_geom(rf.crs, rf.crs, geom, precision=6)
         return geom
 
 
-def train(model):
-    """Train the model."""
-    # Training dataset.
-    dataset_train = BalloonDataset()
-    dataset_train.load_balloon(args.dataset, "train")
-    dataset_train.prepare()
+#def train(model):
+#    """Train the model."""
+#    # Training dataset.
+#    dataset_train = BalloonDataset()
+#    dataset_train.load_balloon(args.dataset, "train")
+#    dataset_train.prepare()
+#
+#    # Validation dataset
+#    dataset_val = BalloonDataset()
+#    dataset_val.load_balloon(args.dataset, "val")
+#    dataset_val.prepare()
+#
+#    # *** This training schedule is an example. Update to your needs ***
+#    # Since we're using a very small dataset, and starting from
+#    # COCO trained weights, we don't need to train too long. Also,
+#    # no need to train all layers, just the heads should do it.
+#    print("Training network heads")
+#    model.train(dataset_train, dataset_val,
+#                learning_rate=config.LEARNING_RATE,
+#                epochs=30,
+#                layers='heads')
 
-    # Validation dataset
-    dataset_val = BalloonDataset()
-    dataset_val.load_balloon(args.dataset, "val")
+############################################################
+#  Training
+############################################################
+
+if __name__ == '__main__':
+
+    COCO_MODEL_PATH = os.path.join("../../mask_rcnn_coco.h5")
+
+    # Create training dataset
+    # Create FieldDataset Object
+    dataset_train = FieldDataset()
+    # Read in the training data
+    dataset_train.load_fields('/Users/dharp/Data/GrandView','train', "/Users/dharp/Data/GrandView/Grandview_fields/Grandview_fields_DW.shp")
+    # Prepare data (load_mask, etc.)
+    dataset_train.prepare()
+    
+    # Create validation dataset
+    # Create FieldDataset Object
+    dataset_val = FieldDataset()
+    # Read in the training data
+    dataset_val.load_fields('/Users/dharp/Data/GrandView','val', "/Users/dharp/Data/GrandView/Grandview_fields/Grandview_fields_DW.shp")
+    # Prepare data (load_mask, etc.)
     dataset_val.prepare()
+
+    config = FieldConfig()
+    config.display()
+
+    model = modellib.MaskRCNN(mode="training", config=config,
+                          model_dir='model')
+
+    model.load_weights(COCO_MODEL_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",
+                                "mrcnn_bbox", "mrcnn_mask"])
 
     # *** This training schedule is an example. Update to your needs ***
     # Since we're using a very small dataset, and starting from
@@ -184,176 +241,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=30,
-                layers='heads')
+                epochs=1,
+                layers='all')
 
 
-def color_splash(image, mask):
-    """Apply color splash effect.
-    image: RGB image [height, width, 3]
-    mask: instance segmentation mask [height, width, instance count]
-
-    Returns result image.
-    """
-    # Make a grayscale copy of the image. The grayscale copy still
-    # has 3 RGB channels, though.
-    gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
-    # Copy color pixels from the original color image where mask is set
-    if mask.shape[-1] > 0:
-        # We're treating all instances as one, so collapse the mask into one layer
-        mask = (np.sum(mask, -1, keepdims=True) >= 1)
-        splash = np.where(mask, image, gray).astype(np.uint8)
-    else:
-        splash = gray.astype(np.uint8)
-    return splash
-
-
-def detect_and_color_splash(model, image_path=None, video_path=None):
-    assert image_path or video_path
-
-    # Image or video?
-    if image_path:
-        # Run model detection and generate the color splash effect
-        print("Running on {}".format(args.image))
-        # Read image
-        image = skimage.io.imread(args.image)
-        # Detect objects
-        r = model.detect([image], verbose=1)[0]
-        # Color splash
-        splash = color_splash(image, r['masks'])
-        # Save output
-        file_name = "splash_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
-        skimage.io.imsave(file_name, splash)
-    elif video_path:
-        import cv2
-        # Video capture
-        vcapture = cv2.VideoCapture(video_path)
-        width = int(vcapture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(vcapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = vcapture.get(cv2.CAP_PROP_FPS)
-
-        # Define codec and create video writer
-        file_name = "splash_{:%Y%m%dT%H%M%S}.avi".format(datetime.datetime.now())
-        vwriter = cv2.VideoWriter(file_name,
-                                  cv2.VideoWriter_fourcc(*'MJPG'),
-                                  fps, (width, height))
-
-        count = 0
-        success = True
-        while success:
-            print("frame: ", count)
-            # Read next image
-            success, image = vcapture.read()
-            if success:
-                # OpenCV returns images as BGR, convert to RGB
-                image = image[..., ::-1]
-                # Detect objects
-                r = model.detect([image], verbose=0)[0]
-                # Color splash
-                splash = color_splash(image, r['masks'])
-                # RGB -> BGR to save image to video
-                splash = splash[..., ::-1]
-                # Add image to video writer
-                vwriter.write(splash)
-                count += 1
-        vwriter.release()
-    print("Saved to ", file_name)
-
-
-############################################################
-#  Training
-############################################################
-
-if __name__ == '__main__':
-    import argparse
-
-    # Parse command line arguments
-    parser = argparse.ArgumentParser(
-        description='Train Mask R-CNN to detect balloons.')
-    parser.add_argument("command",
-                        metavar="<command>",
-                        help="'train' or 'splash'")
-    parser.add_argument('--dataset', required=False,
-                        metavar="/path/to/balloon/dataset/",
-                        help='Directory of the Balloon dataset')
-    parser.add_argument('--weights', required=True,
-                        metavar="/path/to/weights.h5",
-                        help="Path to weights .h5 file or 'coco'")
-    parser.add_argument('--logs', required=False,
-                        default=DEFAULT_LOGS_DIR,
-                        metavar="/path/to/logs/",
-                        help='Logs and checkpoints directory (default=logs/)')
-    parser.add_argument('--image', required=False,
-                        metavar="path or URL to image",
-                        help='Image to apply the color splash effect on')
-    parser.add_argument('--video', required=False,
-                        metavar="path or URL to video",
-                        help='Video to apply the color splash effect on')
-    args = parser.parse_args()
-
-    # Validate arguments
-    if args.command == "train":
-        assert args.dataset, "Argument --dataset is required for training"
-    elif args.command == "splash":
-        assert args.image or args.video,\
-               "Provide --image or --video to apply color splash"
-
-    print("Weights: ", args.weights)
-    print("Dataset: ", args.dataset)
-    print("Logs: ", args.logs)
-
-    # Configurations
-    if args.command == "train":
-        config = BalloonConfig()
-    else:
-        class InferenceConfig(BalloonConfig):
-            # Set batch size to 1 since we'll be running inference on
-            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-            GPU_COUNT = 1
-            IMAGES_PER_GPU = 1
-        config = InferenceConfig()
-    config.display()
-
-    # Create model
-    if args.command == "train":
-        model = modellib.MaskRCNN(mode="training", config=config,
-                                  model_dir=args.logs)
-    else:
-        model = modellib.MaskRCNN(mode="inference", config=config,
-                                  model_dir=args.logs)
-
-    # Select weights file to load
-    if args.weights.lower() == "coco":
-        weights_path = COCO_WEIGHTS_PATH
-        # Download weights file
-        if not os.path.exists(weights_path):
-            utils.download_trained_weights(weights_path)
-    elif args.weights.lower() == "last":
-        # Find last trained weights
-        weights_path = model.find_last()
-    elif args.weights.lower() == "imagenet":
-        # Start from ImageNet trained weights
-        weights_path = model.get_imagenet_weights()
-    else:
-        weights_path = args.weights
-
-    # Load weights
-    print("Loading weights ", weights_path)
-    if args.weights.lower() == "coco":
-        # Exclude the last layers because they require a matching
-        # number of classes
-        model.load_weights(weights_path, by_name=True, exclude=[
-            "mrcnn_class_logits", "mrcnn_bbox_fc",
-            "mrcnn_bbox", "mrcnn_mask"])
-    else:
-        model.load_weights(weights_path, by_name=True)
-
-    # Train or evaluate
-    if args.command == "train":
-        train(model)
-    elif args.command == "splash":
-        detect_and_color_splash(model, image_path=args.image,
-                                video_path=args.video)
-    else:
-        print("'{}' is not recognized. "
-              "Use 'train' or 'splash'".format(args.command))
