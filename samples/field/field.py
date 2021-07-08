3a0c9e955aa3b6c72fbf724557473d1aa0ec02e3
Dylan R Harp <dharp73@gmail.com>
2021-07-08 14:10:38 -0600

    field.py now trains and evaluates

--- samples/field/field.py
+++ samples/field/field.py
@@ -15,6 +15,10 @@ from glob import glob
 from shapely import geometry
 import rasterio.features
 from rasterio.mask import mask
+import random
+from mrcnn.model import log
+from mrcnn import visualize
+import matplotlib.pyplot as plt
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../../")
@@ -204,6 +208,17 @@ def get_raster_bounds(rf):
 #  Training
 ############################################################
 
+def get_ax(rows=1, cols=1, size=8):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+
+    Change the default size attribute to control the size
+    of rendered images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    return ax
+
 if __name__ == '__main__':
 
     COCO_MODEL_PATH = os.path.join("../../mask_rcnn_coco.h5")
@@ -242,6 +257,49 @@ if __name__ == '__main__':
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=1,
-                layers='all')
+                layers='heads')
 
+    model.train(dataset_train, dataset_val, 
+                learning_rate=config.LEARNING_RATE / 10,
+                epochs=2, 
+                layers="all")
 
+    class InferenceConfig(FieldConfig):
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
+
+    inference_config = InferenceConfig()
+
+    # Recreate the model in inference mode
+    model = modellib.MaskRCNN(mode="inference",
+                              config=inference_config,
+                              model_dir="./model")
+
+    # Get path to saved weights
+    # Either set a specific path or find last trained weights
+    # model_path = os.path.join(ROOT_DIR, ".h5 file name here")
+    model_path = model.find_last()
+
+    # Load trained weights
+    print("Loading weights from ", model_path)
+    model.load_weights(model_path, by_name=True)
+
+    # Print results
+    image_id = random.choice(dataset_val.image_ids)
+    original_image, image_meta, gt_class_id, gt_bbox, gt_mask =\
+        modellib.load_image_gt(dataset_val, inference_config,
+                               image_id, use_mini_mask=False)
+    
+    log("original_image", original_image)
+    log("image_meta", image_meta)
+    log("gt_class_id", gt_class_id)
+    log("gt_bbox", gt_bbox)
+    log("gt_mask", gt_mask)
+
+    # Check against mask
+    visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id,
+            dataset_train.class_names, figsize=(8, 8))
+
+    image = dataset_val.load_image(image_id)
+    mask, class_ids = dataset_val.load_mask(image_id)
+    visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
